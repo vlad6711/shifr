4eada9a9302f08ba471a1403e4ad369789569e14
vlad6711 <asafevv0905@gmail.com>
2025-04-09 11:29:42 +0300

    first commit

--- ecryption.py
+++ ecryption.py
@@ -1,4 +1,17 @@
 def encrypt(k, m):
-    return ''.join(map(chr, [x + k for x in map(ord, m)]))
+    return ''.join(map(chr, [(ord(c) + k) % 65536 for c in m]))
 
-print(encrypt(2, 'Hello!'))
+def decrypt(k, m):
+    return ''.join(map(chr, [(ord(c) - k) % 65536 for c in m]))
+
+# Запрос ключа и текста от пользователя
+key = int(input("Введите ключ для шифрования (целое число): "))
+text = input("Введите текст для шифрования: ")
+
+# Шифрование текста
+encrypted_text = encrypt(key, text)
+print("Зашифрованный текст:", encrypted_text)
+
+# Дешифрование текста
+decrypted_text = decrypt(key, encrypted_text)
+print("Расшифрованный текст:", decrypted_text)
